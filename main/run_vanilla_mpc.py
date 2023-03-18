1cfc640a9813b5f68f66c4361f6f3ab8c4c5cabe
Anirudh Aatresh <aaatresh@umich.edu>
2023-03-18 17:57:04 -0400

    Restructured vanilla MPC script.

--- main/run_vanilla_mpc.py
+++ main/run_vanilla_mpc.py
@@ -3,17 +3,31 @@
 """
 
 # Importing necessary libraries
-import numpy as np
+
+## To attach root of this repository to PYTHONPATH
+import sys
+from pathlib import Path
+REPOROOT = Path(__file__).resolve().parents[1]
+sys.path.append(str(REPOROOT))
+
+## For data visualization and plotting
 import matplotlib.pyplot as plt
+
+## Import controller, controller configuration, and plant configuration
 from libs.controllers.controller_config import *
 from libs.servo_mech_system import system_config as servo_system
-from utils.utils import *
 from libs.controllers.vanilla_mpc_def import vanilla_mpc
-import argparse
 
+## Import utils
+from utils.utils import *
+
+## Import setpoint generation function
+from libs.servo_mech_system.setpoint_generator import const_setpoint_gen
+
+## Import argument parser and setup
+import argparse
 parser = argparse.ArgumentParser("Run vanilla MPC in different scenarios. For information about scenarios, refer to README.md.")
 parser.add_argument("-s", type=int, default=None, required=True, help="1 or 2, corresponding to scenario 1 or 2.")
-
 args = parser.parse_args()
 
 # Check value of '-s' argument
@@ -24,14 +38,10 @@ if(not (args.s == 1 or args.s == 2)):
 # Convert the model to discrete-time
 A, B, C = cnt_to_dst(servo_system.Ac, servo_system.Bc, servo_system.C, servo_system.dt)
 
-# Set point
+# Define a constant setpoint value. This can be changed based on the type of setpoint tracking desired. This signal
+# can be made more complex as well.
 r = np.pi / 2
-rt = r * np.ones((Hp, 1))
 
-# Define action model noise standard of deviation
-act_model_std = 3e-2
-# Define action model noise standard of deviation
-sen_model_std = 3e-2
 
 # Initialize yt
 y0 = np.array([[0]])
@@ -39,7 +49,6 @@ next_yt = y0
 
 # Simulation time parameters
 tspan = [0, 20]
-samp_time = 0.1
 
 if(args.s == 1):
     # Weight matrices Rk and Qk
@@ -65,7 +74,7 @@ all_Ys = []
 all_Us = []
 all_covs = []
 
-t_array = np.arange(tspan[0], tspan[1], samp_time)
+t_array = np.arange(tspan[0], tspan[1], servo_system.dt)
 
 # controller
 controller = vanilla_mpc(A, B, C, Rk, Qk, Hu, Hp, act_model_std, sen_model_std, init_Pt, init_xtt_1)
@@ -75,6 +84,9 @@ for e, t in enumerate(t_array):
 
     # collect new data
     yt = next_yt
+
+    # Define setpoint for a certain prediction horizon
+    rt = const_setpoint_gen(r, Hp)
 
     # Calculate optimal control input
     xtt, utt = controller.step(yt, rt)
